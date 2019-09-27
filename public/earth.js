773e3909f7618a3e0c61dd75599f5889ee8c56a4
hjk <hjkaddcit@gmail.com>
2019-09-27 23:44:54 +0200

    =asfvxx

--- public/earth.js
+++ public/earth.js
@@ -13,8 +13,8 @@
 
 	// Earth params
 	var radius = 0.8,
-		segments = 64,
-		rotation = 6;
+		segments = 32,
+		rotation = 2.5;
 
 	var scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xff0000);
@@ -25,13 +25,13 @@
 	var renderer = new THREE.WebGLRenderer( {alpha:true});
 	renderer.setSize(width, height);
 
-	scene.add(new THREE.AmbientLight(0x333333));
-
-
-	//var light = new THREE.DirectionalLight(0xffffff, 1);
-	var light = new THREE.AmbientLight(0xffffff, 3);
+	// Light setup
+	var light = new THREE.DirectionalLight(0xffffff, 0.5);
 	light.position.set(5, 3, 5);
 	scene.add(light);
+
+	scene.add(new THREE.AmbientLight(0xffffff, 1));
+
 
 	var sphere = createSphere(radius, segments);
 	sphere.rotation.y = rotation;
@@ -41,9 +41,6 @@
 	clouds.rotation.y = rotation;
 	scene.add(clouds)
 
-	var stars = createStars(90, 64);
-	//scene.add(stars);
-
 	var controls = new THREE.TrackballControls(camera);
 
 	webglEl.appendChild(renderer.domElement);
@@ -52,8 +49,8 @@
 
 	function render() {
 		controls.update();
-		sphere.rotation.y += 0.0003;
-		clouds.rotation.y += 0.0003;
+		sphere.rotation.y += 0.0005;
+		clouds.rotation.y += 0.0005;
 		requestAnimationFrame(render);
 		renderer.render(scene, camera);
 	}
@@ -65,10 +62,10 @@
 				map: THREE.ImageUtils.loadTexture('https://gmens-test-1.s3.eu-central-1.amazonaws.com/2_no_clouds_4k-removed.png'),
 				transparent: true,
 				side: THREE.DoubleSide,
-				//bumpMap: THREE.ImageUtils.loadTexture('https://gmens-test-1.s3.eu-central-1.amazonaws.com/elev_bump_4k.jpg'),
-				//bumpScale: 0.005,
-				//specularMap: THREE.ImageUtils.loadTexture('https://gmens-test-1.s3.eu-central-1.amazonaws.com/water_4k.png'),
-				//specular: new THREE.Color('grey')
+				bumpMap: THREE.ImageUtils.loadTexture('https://gmens-test-1.s3.eu-central-1.amazonaws.com/elev_bump_4k.jpg'),
+				bumpScale: 0.005,
+				specularMap: THREE.ImageUtils.loadTexture('https://gmens-test-1.s3.eu-central-1.amazonaws.com/water_4k.png'),
+				specular: new THREE.Color('grey')
 			})
 		);
 	}
@@ -80,16 +77,6 @@
 				map: THREE.ImageUtils.loadTexture('https://gmens-test-1.s3.eu-central-1.amazonaws.com/fair_clouds_4k.png'),
 				transparent: true,
 				side: THREE.DoubleSide
-			})
-		);
-	}
-
-	function createStars(radius, segments) {
-		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius, segments, segments),
-			new THREE.MeshBasicMaterial({
-				map: THREE.ImageUtils.loadTexture('https://gmens-test-1.s3.eu-central-1.amazonaws.com/galaxy_starfield.png'),
-				side: THREE.BackSide
 			})
 		);
 	}
